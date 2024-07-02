2ddff6ba4cb1067da3a7b99f0025e831031b0e77
Muhammad Fawdy Renardi Wahyu <87023372+fawdywahyu18@users.noreply.github.com>
2024-07-02 10:29:03 +0700

    Create update_pdrb_jatim.py
    Membuat file helper untuk update PDRB jatim

---
+++ update_pdrb_jatim/update_pdrb_jatim.py
@@ -0,0 +1,58 @@
+"""
+Helper Update data PDRB Seluruh Jatim
+@author: fawdywahyu
+"""
+
+import pandas as pd
+
+def cleaning_data(tahun, kode_kk):
+    # tahun: str, contoh 2016
+    # kode_kk: list berisi str, contoh kode_kepri
+    
+    # tahun = '2016'
+    # kode_kk = kode_analisis_prov
+    
+    pdrb = pd.read_excel(f'Data/PDRB Lapangan Usaha Kab Kota Dalam Milyar Seluruh Indonesia ADHK tahun {tahun}.xlsx')
+
+    lapangan_usaha = list(pdrb.iloc[0,6:])
+    index_kk = list(pdrb[pdrb['Unnamed: 4'].isin(kode_kk)].index)
+    
+    if '9999' in kode_kk:
+        index_kk.sort(reverse=True)
+    
+    nama_kk = list(pdrb['Unnamed: 5'].iloc[index_kk])
+    pdrb_kk = pdrb.iloc[index_kk, 6:]
+    
+    # pdrb_kk = pdrb.iloc[index_kk[0]:(index_kk[-1]+1), 6:]
+    
+    pdrb_kk.columns = lapangan_usaha
+    pdrb_kk['Nama Kab/Kota'] = nama_kk
+    pdrb_kk_melt = pd.melt(pdrb_kk, id_vars=['Nama Kab/Kota'], value_vars=lapangan_usaha)
+    
+    pdrb_kk_melt.columns = ['Nama Kab/Kota', 'Lapangan Usaha', f'Nilai PDRB {tahun}']
+    pdrb_kk_melt[f'Nilai PDRB {tahun}'] = pdrb_kk_melt[f'Nilai PDRB {tahun}'].astype(float)
+    
+    dict_result_cleaning = {
+        'PDRB DF':pdrb_kk_melt,
+        'Lapangan Usaha': lapangan_usaha
+        }
+    return dict_result_cleaning
+
+def creating_pdrb_df(list_tahun, kode_kk_cd):
+    # list_tahun: list, berisi list tahun dari tahun awal sampai tahun akhir
+    # kode_kk: list berisi str, contoh kode_kepri
+    
+    # list_tahun = ['2016', '2019', '2020']
+    # kode_kk_cd = kode_sulsel
+    
+    for t in list_tahun:
+        pdrb_t = cleaning_data(t, kode_kk_cd)['PDRB DF']
+        
+        if t==list_tahun[0]:
+            pdrb_merge = cleaning_data(list_tahun[0], kode_kk_cd)['PDRB DF']
+        else:
+            pdrb_merge = pd.merge(pdrb_merge, pdrb_t,
+                                  on=['Nama Kab/Kota', 'Lapangan Usaha'],
+                                  how='inner')
+    return pdrb_merge
+
