61bb638d2ec5931d441f9855a43fd61793542fc8
Muhammad Fawdy Renardi Wahyu <87023372+fawdywahyu18@users.noreply.github.com>
2024-07-28 20:19:30 +0700

    Update run_pdrb_nganjuk.R
    Revisi error BGVAR

--- run_pdrb_nganjuk.R
+++ run_pdrb_nganjuk.R
@@ -10,14 +10,18 @@ library(tibble)
 library(tvReg)
 library(factoextra)
 
-set.seed(42)
+
+# Rekomendasi: Folder sebaiknya diletakkan di folder 'Documents', sehingga setting directory sebaiknya dilakukan ke folder "Documents"
+# Contoh : 'C:/Users/User/Documents/outlook-Nganjuk-main'
 wd = ''
 setwd(wd)
 pdrb_jatim = read_excel('Data/PDRB Jawa Timur Lapangan Usaha 2010 2022.xlsx') # Data yang selalu diupdate berdasarkan ketersediaan data
+set.seed(42)
 source('helper.R')
 
-hasil_eval_pvar = evaluasi_pvar(pdrb_jatim_input = pdrb_jatim, 2L)
-hasil_eval_bgvar = evaluasi_bgvar(pdrb_jatim_input = pdrb_jatim, 2L)
-hasil_eval_favar = evaluasi_favar(pdrb_jatim_input = pdrb_jatim, 2L)
-out_fcst_pvar = out_forecast_pvar(pdrb_jatim_input = pdrb_jatim, 5L)
-out_fcst_bgvar = out_forecast_bgvar(pdrb_jatim_input = pdrb_jatim, 5L)
+
+hasil_eval_pvar = suppressWarnings({evaluasi_pvar(pdrb_jatim_input = pdrb_jatim, 2L)})
+hasil_eval_bgvar = suppressWarnings({evaluasi_bgvar(pdrb_jatim_input = pdrb_jatim, 2L)})
+hasil_eval_favar = suppressWarnings({evaluasi_favar(pdrb_jatim_input = pdrb_jatim, 2L)})
+out_fcst_pvar = suppressWarnings({out_forecast_pvar(pdrb_jatim_input = pdrb_jatim, 5L)})
+out_fcst_bgvar = suppressWarnings({out_forecast_bgvar(pdrb_jatim_input = pdrb_jatim, 5L)})
