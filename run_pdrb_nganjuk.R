2e526010a504ca1e1b5e31c39e5c15fce497b5b3
Muhammad Fawdy Renardi Wahyu <87023372+fawdywahyu18@users.noreply.github.com>
2024-07-28 13:55:08 +0700

    Update run_pdrb_nganjuk.R
    Edit the location for 'helper.R'

--- run_pdrb_nganjuk.R
+++ run_pdrb_nganjuk.R
@@ -14,7 +14,7 @@ set.seed(42)
 wd = ''
 setwd(wd)
 pdrb_jatim = read_excel('Data/PDRB Jawa Timur Lapangan Usaha 2010 2022.xlsx') # Data yang selalu diupdate berdasarkan ketersediaan data
-source('Code/helper.R')
+source('helper.R')
 
 hasil_eval_pvar = evaluasi_pvar(pdrb_jatim_input = pdrb_jatim, 2L)
 hasil_eval_bgvar = evaluasi_bgvar(pdrb_jatim_input = pdrb_jatim, 2L)
